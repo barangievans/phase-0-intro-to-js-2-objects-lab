02a5890fe402ee81e5ce3ffc6dff89d3d1958fe7
barangievans <barangievans@gmail.com>
2024-07-11 11:32:34 +0300

    intro-to-js

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution in this file!
+// Initialize employee object
+let employee = {
+    name: 'Sam',
+    streetAddress: '11 Broadway'
+  };
+  
+  // Function to update employee non-destructively
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value
+    };
+  }
+  
+  // Function to update employee destructively
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  // Function to delete from employee non-destructively
+  function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  // Function to delete from employee destructively
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
+  module.exports = {
+    updateEmployeeWithKeyAndValue,
+    destructivelyUpdateEmployeeWithKeyAndValue,
+    deleteFromEmployeeByKey,
+    destructivelyDeleteFromEmployeeByKey
+  };
+  
+  
